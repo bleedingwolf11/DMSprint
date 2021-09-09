64c0547cbc06c35f1c26bbec565b42635933fb93
bleedingwolf11 <56654525+bleedingwolf11@users.noreply.github.com>
2021-09-09 12:54:39 +0530

    dd

--- src/test/java/springBootDM/SpringBootDockerMavenApplicationTests.java
+++ src/test/java/springBootDM/SpringBootDockerMavenApplicationTests.java
@@ -10,8 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringBootDockerMavenApplicationTests {
 
 	@Test
-	public void contextLoads() {
-	System.out.print("Hello Wolrd");
+	public String contextLoads() {
+	
+	return "Helo world";
 	}
 
 }
